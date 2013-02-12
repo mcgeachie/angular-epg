c75dfe005de18c40bffe9349758b04055445566d
telly <telly@bskyb.com>
2013-02-12 11:39:35 +0000

    ME|RF: Fang! - yeoman initialized project

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var fangApp = angular.module('fangApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+fangApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
