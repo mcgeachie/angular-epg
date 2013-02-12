6b8f487e3ee8311198099eb59343dccc1a483030
telly <telly@bskyb.com>
2013-02-12 16:45:03 +0000

    ME|RF: Fang! - minify-proof the controller so we know it's working when deployed

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,9 @@
 'use strict';
 
-fangApp.controller('MainCtrl', function($scope) {
+fangApp.controller('MainCtrl', ['$scope', function($scope) {
   $scope.awesomeThings = [
     'HTML5 Boilerplate',
     'AngularJS',
     'Testacular'
   ];
-});
+}]);
