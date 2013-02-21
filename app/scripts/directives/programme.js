bf685dad3670b8166066b481e5d39dde5aca9c44
telly <dl-skyplusonline@bskyb.com>
2013-02-21 09:54:14 +0000

    RW|RF: Fang! - fix position and width of programmes in IE

--- app/scripts/directives/programme.js
+++ app/scripts/directives/programme.js
@@ -1,11 +1,15 @@
 'use strict';
 
 angular.module('epgDirectives', ['epgFilters'])
-    .directive('programme', function () {
+    .directive('programme', ['$filter', function ($filter) {
         return {
-            template: '<a href="#" class="epg-programme" style="width: {{programme.m[1] | durationInPixels}}; left: {{programme.s | startTimeInPixels}}" data-ng-class="{short: programme.m[1] <= 900}">{{programme.t}}</a>',
+            template: '<a href="#" class="epg-programme" data-ng-class="{short: programme.m[1] <= 900}">{{programme.t}}</a>',
             restrict: 'E',
-            replace: true
+            replace: true,
+            link: function (scope, element) {
+                element.css('width', $filter('durationInPixels')(scope.programme.m[1]))
+                    .css('left', $filter('startTimeInPixels')(scope.programme.s));
+            }
         };
-    });
+    }]);
 
